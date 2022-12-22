768f39a1c3b9d3fbcd6624c271cdfed00a30a164
Ashley (Scirra) <ashley@scirra.com>
2022-12-22 19:52:15 +0000

    Add extra turret aim range
    Now turrets have an extra secondary range, in which they will aim at the nearest target if there's nothing in their primary firing range. This helps ensure turrets are primed pointing in the right direction when a target first enters range.

--- files/gameServer/units/unitTurret.js
+++ files/gameServer/units/unitTurret.js
@@ -9,6 +9,11 @@ const _2PI = 2 * Math.PI;
 // currently set to 0.5 degrees.
 const TURRET_SEND_ANGLE_THRESHOLD = MathUtils.ToRadians(0.5);
 
+// Units outside of firing range, but inside the aiming range, are aimed at but not fired
+// at. This makes turrets aim at nearby units that are still out of range so they are
+// ready if it does come in to range. This extra range is a percentage on the normal range.
+const AIM_EXTRA_RANGE = 1.5;		// 50% extra range to aim at
+
 // A UnitTurret is the firing part of a unit, that rotates independently of the platform
 // to aim and fire projectiles at other player's units.
 // Note that importantly the UnitTurret position and angle are treated as offsets
@@ -18,9 +23,9 @@ export class UnitTurret extends PositionedAndAngledObject {
 
 	// Private fields
 	#unit;					// reference to Unit
-	#targetUnitId = -1;		// ID of unit turret is targeting, or -1 if none
+	#targetUnitId = -1;		// ID of unit turret is targeting to fire at, or -1 if none
 	
-	#range = 1400;			// maximum range to identify a target
+	#range = 1400;			// maximum range to fire at a target
 	#rotateSpeed = MathUtils.ToRadians(70);
 	
 	// The accuracy is a random adjustment to the projectile angle when firing.
@@ -116,14 +121,19 @@ export class UnitTurret extends PositionedAndAngledObject {
 	
 	Tick(dt)
 	{
+		// If there is no target to fire at, try to find a target. This can also rotate the turret
+		// towards targets that are outside of firing range, but inside the aim range.
+		// Otherwise if a target inside firing range has been identified, track that and fire at it.
 		if (this.#targetUnitId === -1)
 			this.#FindTarget(dt);
 		else
 			this.#TrackTarget(dt);
 	}
 	
-	// Identify if there is a unit this turret can target in range.
-	// If there are multiple targets, it will try to find the best one to target.
+	// Identify firstly if there is a unit this turret can fire at in range.
+	// Secondly if there is nothing inside its firing range, it will rotate towards anything
+	// inside its aim range. In both cases if there are multiple targets, it will try to find
+	// the best one to target, based on different criteria (rotation vs. distance).
 	#FindTarget(dt)
 	{
 		const forPlayer = this.GetUnit().GetPlayer();
@@ -134,19 +144,26 @@ export class UnitTurret extends PositionedAndAngledObject {
 		// at a target that can't fire back.
 		const [fromX, fromY] = this.GetPlatform().GetPosition();
 		
-		// Collect all available targets in range.
-		const availableTargets = [];
+		// Collect all available targets within firing range and aiming range separately.
+		// Firing range takes priority, as there's no point merely aiming at a target outside of
+		// firing range when there's a target that could be fired at.
+		const fireTargets = [];
+		const aimTargets = [];
+		
+		// The turret's main range is the distance at which it can fire at targets. Calculate an additional
+		// range in which the turret can aim at targets if there is nothing to fire at.
+		const aimRange = this.#range * AIM_EXTRA_RANGE;
 		
 		// Iterating every unit in the game would be extremely inefficient, leading to an O(n^2) number
-		// of checks. Instead only check the contents of the collision cells that the range covers.
+		// of checks. Instead only check the contents of the collision cells that the aim range covers.
 		// This covers a square area that may be larger than the actual range circle, but it checks
-		// the circular range inside the callback, and the most important thing is it will skip
+		// the circular ranges inside the callback, and the most important thing is it will skip
 		// most units in the game, making it far more efficient. Also note that ForEachItemInArea()
 		// can run the callback multiple times with the same unit platform. However this does not
-		// matter; if availableTargets has duplicates, it will fall through to picking the best one
-		// by nearest angular difference to the turret, and that algorithm works the same with duplicates.
+		// matter; if there are duplicate targets, it will fall through to picking the best one
+		// either by angle or distance, and those algorithms works the same with duplicates.
 		this.GetGameServer().GetCollisionGrid().ForEachItemInArea(
-			fromX - this.#range, fromY - this.#range, fromX + this.#range, fromY + this.#range,
+			fromX - aimRange, fromY - aimRange, fromX + aimRange, fromY + aimRange,
 			unitPlatform =>
 			{
 				// Skip units from the same player
@@ -159,61 +176,125 @@ export class UnitTurret extends PositionedAndAngledObject {
 				const [toX, toY] = unitPlatform.GetPosition();
 				const dx = toX - fromX;
 				const dy = toY - fromY;
-				if (dx * dx + dy * dy <= this.#range * this.#range)
+				const sqDist = dx * dx + dy * dy;
+				if (sqDist <= this.#range * this.#range)
 				{
-					// Found a target. Add it to the list of available targets.
-					// Keep looking for further available targets.
-					availableTargets.push(unit);
+					// Found a target in firing range. Add it to the list of available
+					// targets to fire at, and keep looking for further targets.
+					fireTargets.push(unit);
+				}
+				else if (sqDist <= aimRange * aimRange)
+				{
+					// This target is not in firing range, but is inside the aim range.
+					// Add it to the list of available aim targets and keep looking.
+					aimTargets.push(unit);
 				}
 			});
 		
-		// Did not find a target. Rotate the turret back to angle 0,
-		// so it returns to its default orientation with the unit platform.
-		if (availableTargets.length === 0)
+		// Did not find any target in firing range. In this case, fall back to checking targets
+		// inside the larger aim range.
+		if (fireTargets.length === 0)
 		{
-			this.SetAngle(MathUtils.AngleRotate(this.GetAngle(), 0, this.#rotateSpeed * dt));
+			// No targets in aim range either. Rotate the turret back to angle 0,
+			// so it returns to its default orientation with the unit platform.
+			if (aimTargets.length === 0)
+			{
+				this.SetAngle(MathUtils.AngleRotate(this.GetAngle(), 0, this.#rotateSpeed * dt));
+			}
+			// One target in aim range. Just aim for that.
+			else if (aimTargets.length === 1)
+			{
+				this.#RotateTowardsTarget(aimTargets[0], dt);
+			}
+			// Multiple targets are available in the aim range.
+			else
+			{
+				// Find the best available target based on closest by distance.
+				const bestAimTarget = this.#GetBestAvailableTargetByDistance(aimTargets);
+				this.#RotateTowardsTarget(bestAimTarget, dt);
+			}
 		}
-		// Only found one available target. Just aim for that.
-		else if (availableTargets.length === 1)
+		// Only found one available target in firing range. Just target that.
+		else if (fireTargets.length === 1)
 		{
-			this.#targetUnitId = availableTargets[0].GetId();
+			this.#targetUnitId = fireTargets[0].GetId();
 		}
-		// Multiple targets are available in range.
+		// Multiple targets are available in the firing range.
 		else
 		{
-			// When multiple targets are available, aim for the one that requires the least
-			// rotation by the turret. This avoids things like rotating the turret all the way around
-			// in the opposite direction to fire at something behind the unit when there is already
-			// a target in front of the unit, saving time in combat. Therefore we need to find the
-			// target with the minimum angular distance from the current turret angle.
-			// Note the availableTargets array could contain duplicates due to the way collision cells
-			// work; however this does not matter with this algorithm, it will work the same anyway.
-			const turretAngle = this.GetOverallAngle();
-			
-			// Find the "best" target, i.e. the one with the least angular distance from the turret.
-			let bestTargetUnit = null;
-			let bestTargetAngleDist = Infinity;		// start with Infinity so first target always beats this
-			
-			for (const unit of availableTargets)
-			{
-				// Find this available target's angular distance from the turret angle.
-				const [targetX, targetY] = unit.GetPlatform().GetPosition();
-				const angleToTarget = MathUtils.AngleTo(fromX, fromY, targetX, targetY);
-				const angleDist = MathUtils.AngleDifference(turretAngle, angleToTarget);
-				
-				// If this target has a smaller angular distance to the turret angle than the current best,
-				// then replace it as the best target.
-				if (angleDist < bestTargetAngleDist)
-				{
-					bestTargetUnit = unit;
-					bestTargetAngleDist = angleDist;
-				}
-			}
-			
-			// Now the best target is set to the available target with the smallest angular distance to the turret's current angle.
-			// Start targeting that unit.
-			this.#targetUnitId = bestTargetUnit.GetId();
+			// Find the best available target requiring least rotation to aim at.
+			this.#targetUnitId = this.#GetBestAvailableTargetByRotation(fireTargets).GetId();
 		}
+	}
+	
+	// When multiple targets are available in firing range, aim for the one that requires the
+	// least turret rotation. This avoids things like rotating the turret all the way around
+	// in the opposite direction to fire at something behind the unit when there is already
+	// a target in front of the unit, saving time in combat. Therefore we need to find the
+	// target with the minimum angular distance from the current turret angle.
+	// Note the availableTargets array could contain duplicates due to the way collision cells
+	// work; however this does not matter with this algorithm, it will work the same anyway.
+	#GetBestAvailableTargetByRotation(availableTargets)
+	{
+		const turretAngle = this.GetOverallAngle();
+		const [fromX, fromY] = this.GetPlatform().GetPosition();
+
+		// Find the best target by rotation, i.e. the one with the least angular distance from the turret.
+		let bestTargetUnit = null;
+		let bestTargetAngleDist = Infinity;		// start with Infinity so first target always beats this
+
+		for (const unit of availableTargets)
+		{
+			// Find this available target's angular distance from the turret angle.
+			const [targetX, targetY] = unit.GetPlatform().GetPosition();
+			const angleToTarget = MathUtils.AngleTo(fromX, fromY, targetX, targetY);
+			const angleDist = MathUtils.AngleDifference(turretAngle, angleToTarget);
+
+			// If this target has a smaller angular distance to the turret angle than the current best,
+			// then replace it as the best target.
+			if (angleDist < bestTargetAngleDist)
+			{
+				bestTargetUnit = unit;
+				bestTargetAngleDist = angleDist;
+			}
+		}
+		
+		// Now the best target is set to the available target with the smallest angular distance to
+		// the turret's current angle. Return that target.
+		return bestTargetUnit;
+	}
+	
+	// If there are no targets in firing range but multiple targets within the extended aim range,
+	// then the best target to aim at is the nearest one. This is based on the assumption the nearest
+	// target is the most likely to enter the firing range first. This works similarly to the previous
+	// method but by distance instead of rotation.
+	#GetBestAvailableTargetByDistance(availableTargets)
+	{
+		const [fromX, fromY] = this.GetPlatform().GetPosition();
+		
+		// Find the best target by distance, i.e. the one closest to the unit.
+		let bestTargetUnit = null;
+		let bestTargetSqDist = Infinity;		// start with Infinity so first target always beats this
+
+		for (const unit of availableTargets)
+		{
+			// Find the square distance to this target. This avoids having to compute a square root.
+			// The nearest target will still have the lowest distance squared.
+			const [targetX, targetY] = unit.GetPlatform().GetPosition();
+			const dx = targetX - fromX;
+			const dy = targetY - fromY;
+			const sqDist = dx * dx + dy * dy;
+
+			// Found a nearer target than the current best.
+			if (sqDist < bestTargetSqDist)
+			{
+				bestTargetUnit = unit;
+				bestTargetSqDist = sqDist;
+			}
+		}
+		
+		// Return the nearest available target.
+		return bestTargetUnit;
 	}
 	
 	#TrackTarget(dt)
@@ -231,10 +312,9 @@ export class UnitTurret extends PositionedAndAngledObject {
 			return;
 		}
 		
-		// Check the target is still in range.
+		// Check the target is still in firing range.
 		const [platformX, platformY] = this.GetPlatform().GetPosition();
-		const targetPlatform = unit.GetPlatform();
-		const [targetX, targetY] = targetPlatform.GetPosition();
+		const [targetX, targetY] = unit.GetPlatform().GetPosition();
 		const dx = targetX - platformX;
 		const dy = targetY - platformY;
 		if (dx * dx + dy * dy > this.#range * this.#range)
@@ -242,6 +322,24 @@ export class UnitTurret extends PositionedAndAngledObject {
 			this.#targetUnitId = -1;
 			return;
 		}
+		
+		// Rotate towards the target, returning the angle to the target and the angle
+		// after rotation.
+		const { targetAngle, rotatedAngle } = this.#RotateTowardsTarget(unit, dt);
+		
+		// If the turret is now facing at its target (within a tiny threshold to allow for
+		// precision errors), and it has reloaded, then fire a projectile.
+		if (MathUtils.AngleDifference(targetAngle, rotatedAngle) < MathUtils.ToRadians(0.01) &&
+			gameServer.GetGameTime() >= this.#lastFireTime + this.#rateOfFire)
+		{
+			this.#FireProjectile();
+		}
+	}
+	
+	#RotateTowardsTarget(unit, dt)
+	{
+		const targetPlatform = unit.GetPlatform();
+		const [targetX, targetY] = targetPlatform.GetPosition();
 		
 		// Get the angle to the target. Use predictive aim to find the angle to aim at,
 		// which takes in to account the projectile speed and target speed and angle, to make
@@ -259,13 +357,8 @@ export class UnitTurret extends PositionedAndAngledObject {
 		const rotatedAngle = MathUtils.AngleRotate(myAngle, targetAngle, this.#rotateSpeed * dt);
 		this.SetAngle(rotatedAngle - platformAngle);
 		
-		// If the turret is now facing at its target (within a tiny threshold to allow for
-		// precision errors), and it has reloaded, then fire a projectile.
-		if (MathUtils.AngleDifference(targetAngle, rotatedAngle) < MathUtils.ToRadians(0.01) &&
-			gameServer.GetGameTime() >= this.#lastFireTime + this.#rateOfFire)
-		{
-			this.#FireProjectile();
-		}
+		// Return the target angle and rotated angle.
+		return { targetAngle, rotatedAngle };
 	}
 	
 	// Called when the turret is to shoot its projectile.
