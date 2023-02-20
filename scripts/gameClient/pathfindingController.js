de71bec35603b25248e2b19d58981878c3bb92fc
Ashley (Scirra) <ashley@scirra.com>
2023-02-20 00:31:53 +0000

    Simplify precise destination
    Simplify previous code for precise movement destination using the new IsCellObstacle() helper method from the previous commit.

--- scripts/gameClient/pathfindingController.js
+++ scripts/gameClient/pathfindingController.js
@@ -74,12 +74,9 @@ export class PathfindingController {
 			// If the destination position cell is not an obstacle, replace the last node with
 			// the specified destination position. This allows moving to the exact location
 			// rather than the middle of the nearest cell at the destination position.
-			const lastNode = nodeList.at(-1);
-			const pfMap = this.#pathfindingBeh.map;
-			const cellSize = pfMap.cellSize;
-			
-			if (!pfMap.isCellObstacle(Math.floor(toX / cellSize), Math.floor(toY / cellSize)))
+			if (!this.IsCellObstacle(toX, toY))
 			{
+				const lastNode = nodeList.at(-1);
 				lastNode[0] = toX;
 				lastNode[1] = toY;
 			}
