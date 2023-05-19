cc24fb438089bdccfa99c8d2ff87a2a807d96bb1
Ashley (Scirra) <ashley@scirra.com>
2023-05-19 14:39:39 +0100

    [Ignore] Investigating GitHub bug
    A test commit solely for trying to reproduce an apparent bug in GitHub. Unrelated to Command & Construct.

---
+++ test.js
@@ -0,0 +1,192 @@
+function test()
+{
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+
+	console.log("Hello world");
+	
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+	// filler
+}
+
+class Test {
+	method()
+	{
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		
+		console.log("Hello world");
+		
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+	}
+}
+
+export class ExportTest {
+	exportMethod()
+	{
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		
+		console.log("Hello world");
+		
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+		// filler
+	}
+}
